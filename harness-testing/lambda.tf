d8b2b11bfffd6a322a6e1e47b24a084b520aff3f
veerabhadraswamy17 <veerabhadraswamy@outlook.com>
2024-07-06 21:18:34 +0530

    updated lamda

--- harness-testing/lambda.tf
+++ harness-testing/lambda.tf
@@ -1,38 +1,46 @@
-# data "archive_file" "lambda_zip" {
-#   type        = "zip"
-#   source_file = "lambda_function.py"
-#   output_path = "lambda_function.zip"
-# }
+resource "aws_lambda_function" "hello_world" {
+  function_name = "hello_world_lambda"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  
+  filename      = "lambda.zip"
 
-resource "aws_iam_role" "lambda_exec_role" {
+  source_code_hash = filebase64sha256("lambda.zip")
+}
+
+resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
         Action = "sts:AssumeRole",
         Principal = {
-          Service = "lambda.amazonaws.com",
+          Service = "lambda.amazonaws.com"
         },
-        Effect   = "Allow",
-        Sid      = "",
+        Effect = "Allow",
+        Sid    = ""
       },
     ],
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_lambda_function" "hello_world" {
-  function_name = "hello_world_lambda"
-  role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.8"
-  filename      = "harness-testing/lambda_function.zip"
-
-  source_code_hash = filebase64sha256("harness-testing/lambda_function.zip")
+resource "aws_iam_role_policy" "lambda_exec_policy" {
+  name = "lambda_exec_policy"
+  role = aws_iam_role.lambda_exec.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      },
+    ],
+  })
 }
