20714754e97cd3653addd6969ef2032b9ac48d1b
veerabhadraswamy17 <veerabhadraswamy@outlook.com>
2024-07-06 19:17:23 +0530

    adding s3 code

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,11 @@ provider "aws" {
 }
 
 # Create an S3 bucket
-resource "aws_s3_bucket" "my_harness_bucket" {
+resource "aws_s3_bucket" "my_harness_tfstate_bucket" {
   bucket = "my-unique-harness-bucket-name" # specify a unique bucket name
   
   tags = {
-    Name        = "My harness bucket"
-    Environment = "Harness state file"
+    Name        = "harness tf bucket"
+    Environment = "Harness tf state file"
   }
 }
