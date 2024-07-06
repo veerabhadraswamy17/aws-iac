6089be28470150776c39d391c7975b36eabd6e45
veerabhadraswamy17 <veerabhadraswamy@outlook.com>
2024-07-06 20:36:59 +0530

    adding simple lambda function

--- harness-testing/lambda.tf
+++ harness-testing/lambda.tf
@@ -32,7 +32,7 @@ resource "aws_lambda_function" "hello_world" {
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
-  filename      = "lambda_function.zip"
+  filename      = "${path.module}/lambda_function.zip"
 
-  source_code_hash = filebase64sha256("lambda_function.zip")
+  source_code_hash = filebase64sha256("${path.module}/lambda_function.zip")
 }
