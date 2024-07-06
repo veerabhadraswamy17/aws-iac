c4a6d003caf0f622f1df789990363a91eaa7701b
veerabhadraswamy17 <veerabhadraswamy@outlook.com>
2024-07-06 19:36:14 +0530

    adding vpc code

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,16 +1,4 @@
 # Configure the AWS provider
-
-terraform {
-    required_providers {
-      aws ={
-        source = "hashicorp/aws"
-        version = "5.0"
-      }
-    }
-    backend "s3" {
-    }
-
-}
 provider "aws" {
   region = "us-west-2" # specify your preferred region
 }
